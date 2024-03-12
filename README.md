0c8647e430dba0842a3f3cfd5d1230c473efff4f
Kim Engse <error302@naver.com>
2024-03-12 13:04:35 +0900

    24.03.12 v6

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ React using VS Code
   ![Static Badge](https://img.shields.io/badge/-Visual%20Studio%20Code-007ACC?logo=visualstudiocode)
 
 # What did I study?
+- Import, Export modules
--- my-app-240312/src/01/MyClockImage.js
+++
@@ -1,9 +0,0 @@
-import logo from "../logo.svg";
-
-function MyClockImage() {
-  return (
-      <img src={logo} className="App-logo" alt="logo" />
-  );
-}
-
-export default MyClockImage;
