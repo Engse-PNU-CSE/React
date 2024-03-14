80137727561034d134d3e1dcc090a96ee3658996
Kim Engse <error302@naver.com>
2024-03-14 12:57:14 +0900

    24.03.14 v4

--- src/02/HelloWhite.js
+++ src/02/HelloWhite.js
@@ -1,20 +1,22 @@
 import React from "react";
-import logo from "../logo.svg";
 import styles from "./HelloWhite.module.css"
 
 export default function Hellowhite() {
-  const helloDisign = {
-    backgroundColor : 'white',
-    color : 'red',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
+
+  const apikey = process.env.REACT_APP_MY_API
+  console.log(apikey)
   return (
-    <div styles = {helloDisign} >
-      <h1>Hello, React</h1>
-      <p className={styles.ch1} >Hello, VSCode</p>
+    <div className='flex flex-col w-full max-w-screen-xl mx-auto h-screen overscroll-y-auto'>
+      <header className='flex justify-between items-center text-xl font-bold h-20 p-10 bg-slate-100'>
+        <h1>Hello, React</h1>
+        <h1>Home</h1>
+      </header>
+      <main>
+        <p className= 'flex justify-center items-center' >Hello, VSCode</p>
+      </main>
+      <footer className='flex justify-center items-center text-cyan-50 bg-cyan-950 h-20'>
+        © 2024 KimEngse, CSE-PNU. All rights reserved.
+      </footer>
     </div>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import MyClock from './01/MyClock';
+import Hellowhite from './02/HelloWhite';
 function App() {
   return (
     <div className="App">
-      <div className='flex flex-col w-full max-w-screen-xl h-screen mx-auto overscroll-auto'>
-      <header className='flex justify-between h-20 bg-sky-200 text-xl font-bold'>
-        <div>React Train2</div>
-        <div>Home</div>
-      </header>
-      <main>
-        Main
-      </main>
-      <footer className='flex justify-center items-center text-white bg-slate-800 h-20'>
-        Footer
-      </footer>
-    </div>
+      <MyClock />
+      <Hellowhite />
     </div>
   );
 }
