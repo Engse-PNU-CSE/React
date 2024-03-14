89adf11eaeb4fe72b3970c9e60c531cf173c92bd
Kim Engse <error302@naver.com>
2024-03-14 13:08:14 +0900

    24.03.14 v5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
-import MyClock from './01/MyClock';
-import Hellowhite from './02/HelloWhite';
+import "./App.css";
+import MyClock from "./01/MyClock";
 function App() {
   return (
-    <div className="App">
+    <div className="flex flex-col w-full max-w-screen-xl mx-auto h-screen overscroll-y-auto">
       <MyClock />
-      <Hellowhite />
+      <header className="flex justify-between items-center text-xl font-bold h-20 p-10 bg-slate-100">
+        <h1>Hello, React</h1>
+        <h1>Home</h1>
+      </header>
+      <main>
+        <p className="flex justify-center items-center">Hello, VSCode</p>
+      </main>
+      <footer className="flex justify-center items-center text-cyan-50 bg-cyan-950 h-20">
+        © 2024 KimEngse, CSE-PNU. All rights reserved.
+      </footer>
     </div>
   );
 }
